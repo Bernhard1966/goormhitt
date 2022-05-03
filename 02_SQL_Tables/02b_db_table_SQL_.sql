8b6ece561be28ea806a220f0fc01f7aec855bd1d
Bernhard1966 <b.vonWangenheim@gmail.com>
2022-05-03 19:35:22 +0000

    ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the  right syntax to use near '* -----

--- 02_SQL_Tables/02b_db_table_SQL_.sql
+++ 02_SQL_Tables/02b_db_table_SQL_.sql
@@ -6,40 +6,34 @@
 
 
 /* Datenbank auf Server anzeigen */
-SHOW DATABASES;
-
-
+#SHOW DATABASES;
 
 /* DB boo löschen, falls vorhanden*/
 DROP DATABASE IF EXISTS boo;
 
-
-
 /* DB boo anlegen, falls noch nicht vorhanden*/
 CREATE DATABASE IF NOT EXISTS boo;
 
-
-
-SHOW DATABASES;
-
-
+#SHOW DATABASES;
 
 /* DB auswählen */
 USE boo;
 
 
 
-CREATE TABLE test
+CREATE TABLE IF NOT EXISTS test
 (
-name VARCHAR(20),
-age INT
+name VARCHAR(20) NOT null DEFAULT  "TBA",
+age INT NOT null DEFAULT 0 
 
 );
 
+#SHOW TABLES;
+DESCRIBE test;
 INSERT INTO test(name,age) VALUES ( " Grizabella " , 29 );
 INSERT INTO test(age,name) VALUES ( 35 , " Alonzo " );
-
-
+INSERT INTO test(name,age) VALUES ( " otto " , 21 );
+INSERT INTO test(name,age) VALUES ( " Karla " , 9 );
 
 /* Alle Tabellen in der DB anzeigen */
 SHOW TABLES;
@@ -48,3 +42,4 @@ SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
 DESCRIBE test;
+SELECT * FROM test;
